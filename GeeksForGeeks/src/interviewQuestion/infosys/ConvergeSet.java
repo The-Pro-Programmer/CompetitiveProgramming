cf7e4faf528e60291734791b264887749dde4bf7
Mansi Wakharkar <mansiswakharkar@gmail.com>
2021-08-04 23:42:08 +0300

    Converge set in java

--- GeeksForGeeks/src/interviewQuestion/infosys/ConvergeSet.java
+++ GeeksForGeeks/src/interviewQuestion/infosys/ConvergeSet.java
@@ -1,21 +1,36 @@
 package interviewQuestion.infosys;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ConvergeSet {
 	
 	//Program  asked in Infosys interview question
 	//Process number n --> if n is even there return n/2 else return (n*3)+1
 	
+	static Map<Integer, List<Integer>> directory = new HashMap<>();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		int arr[] = new int[] {1, 2, 8, 10};
-		Arrays.stream(arr).forEach(num -> displayConvergeSet(num));
-		
+		Arrays.stream(arr).forEach(num -> displayConvergeSet2(num));
+	}
+	
+	static void displayConvergeSet2(int num) {
+		Set<Integer> convergerSet = retrieveConvergeSet2(num);
+		System.out.println(num);
+		convergerSet.stream().forEach(n -> System.out.print(n + " "));
+		System.out.println();
 	}
 
-	private static void displayConvergeSet(int num) {
+	static Set<Integer> retrieveConvergeSet2(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	static void displayConvergeSet(int num) {
 		String str = retrieveConvergeSet(num);
 		System.out.println(num + " : {" + str + "}");
 	}
